70c076589b6c511aed911e4933a59e35baf7cf69
Kaviya S <kaviya.s0806@gmail.com>
2025-02-02 09:41:39 +0530

    Maximum value in array

---
+++ MaxValueArray.java
@@ -0,0 +1,35 @@
+// Maximum value of an array
+
+public class MaxValueArray{
+
+
+public static void main(String[] args){
+
+int[] arr = {1,44,78,18,5};
+
+System.out.print(max(arr));
+
+
+}
+
+
+
+static int max(int[] arr){
+
+int maxval = arr[0];
+
+for(int i =1; i < arr.length; i++){
+
+if(arr[i] > maxval)
+
+maxval = arr[i];
+
+}
+return maxval;
+
+
+}
+
+
+
+}
