7d0a09423bbe6e2aa95e299310350eccbf075b3c
Kaviya S <kaviya.s0806@gmail.com>
2025-02-11 21:36:58 +0530

    Create Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,36 @@
+// string palindrome
+
+
+public class Palindrome{
+
+public static void main(String[] args){
+
+String str = "racecar";
+
+System.out.println(isPalindrome(str));
+
+}
+
+static boolean isPalindrome(String str){
+
+int start = 0;
+
+int end = str.length()-1; 
+
+while(start < end){
+
+if(str.charAt(start) != str.charAt(end)){
+
+return false;
+
+}
+
+start++;
+
+end--;
+
+}
+return true;
+
+}
+}
