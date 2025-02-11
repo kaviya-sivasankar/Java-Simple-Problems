653022a093cd5d5454efeba71ef4b30cee3918cf
Kaviya S <kaviya.s0806@gmail.com>
2025-02-12 00:26:45 +0530

    Add files via upload

---
+++ SolutionString.java
@@ -0,0 +1,14 @@
+
+
+public class SolutionString{
+
+  public static void main(String[] args){
+
+
+     StringBuilder str = new StringBuilder("kaviya");
+
+     str.append("sivasankar");
+
+     System.out.println(str);
+   }
+}
