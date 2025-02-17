fb632fbdbdd3128fca40bf7ac7e9bfe877c34075
Kaviya S <kaviya.s0806@gmail.com>
2025-02-17 22:21:41 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -1,26 +1,25 @@
-//reverse 
+//Reversed right angled triangle-number pattern 
 
 import java.util.Scanner;
 
 public class Pattern2{
 
-public static void main(String[] args){
+   public static void main(String[] args){
 
-Scanner scanner = new Scanner(System.in);
+     Scanner scanner = new Scanner(System.in);
 
-System.out.print("enter the number: ");
+     System.out.print("enter the number: ");
 
-int num =scanner.nextInt();
+     int num =scanner.nextInt();
 
-for(int row = 1;row<=num;row++){
+  for(int row = 1;row<=num;row++){
 
-for(int column =1;column<=num-row+1;column++){
+   for(int column =1;column<=num-row+1;column++){
 
-System.out.print(column);
+      System.out.print(column);
 }
 
-System.out.println();
-
-}
-}
+  System.out.println();
+   }
+ }
 }
