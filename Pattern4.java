d1a432a181ef7bac3684427351c44f260e043342
Kaviya S <kaviya.s0806@gmail.com>
2025-02-21 01:23:23 +0530

    Pattern4.java

--- Pattern4.java
+++ Pattern4.java
@@ -1,38 +1,32 @@
 import java.util.Scanner;
 
-public class Pattern4{
+  public class Pattern4{
 
-public static void main(String[] args){
+     public static void main(String[] args){
 
-Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-System.out.print("Enter a number: ");
+        System.out.print("Enter a number: ");
 
-int num = scanner.nextInt();
+        int num = scanner.nextInt();
 
-for(int row = 0; row<2*num;row++){
+     for(int row = 0; row<2*num;row++){
 
-int totColInRow = row > num? 2*num - row:row;
+        int totColInRow = row > num? 2*num - row:row;
 
+        int totNoOfSpace = num - totColInRow;
 
-int totNoOfSpace = num - totColInRow;
+    for(int space =0; space<=totNoOfSpace;space++){
 
-for(int space =0; space<=totNoOfSpace;space++){
+       System.out.print(" ");
+   }
 
-System.out.print(" ");
+    for(int column = 0; column<totColInRow; column++){
 
+      System.out.print("* ");
 }
 
-for(int column = 0; column<totColInRow; column++){
-
-System.out.print("* ");
-
-}
-
-System.out.println();
-
-}
-
-}
-
+      System.out.println();
+    }
+  }
 }
