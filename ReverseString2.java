6b2b7eb25c38d5a4c51992f5aafa70df343965a8
Kaviya S <kaviya.s0806@gmail.com>
2025-02-26 23:20:26 +0530

    Add files via upload

---
+++ ReverseString2.java
@@ -0,0 +1,39 @@
+//reverse string efficient for large string
+
+
+//efficient reversestring method for large string
+
+public class ReverseString2{
+
+   public static void main(String[] args){
+
+
+     String str = "HelloWorld";
+
+     char[] charArray = str.toCharArray();
+
+     int left = 0;
+
+     int right = charArray.length-1;
+
+   while(left < right){
+
+     char temp = charArray[left];
+
+     charArray[left] = charArray[right];
+
+     charArray[right] = temp;
+
+   left++;
+
+   right--;
+
+}
+
+  String reversedStr = new String(charArray);
+
+  System.out.println(reversedStr);
+
+ }
+}
+
