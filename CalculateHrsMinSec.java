e44a07ba622db8b220ab4383219182542cce1d71
Kaviya S <kaviya.s0806@gmail.com>
2025-03-02 19:51:18 +0530

    CalculateHrsMinSec.java

--- CalculateHrsMinSec.java
+++ CalculateHrsMinSec.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
-public class CalculateHrsMinSec{
+  public class CalculateHrsMinSec{
 
-public static void main(String[] args){
+     public static void main(String[] args){
 
-Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-System.out.print("Enter the noOfDays: ");
+        System.out.print("Enter the noOfDays: ");
 
-int days = sc.nextInt();
+      int days = sc.nextInt();
 
-int hours = days * 24;
+      int hours = days * 24;
 
-int min = days * 24 * 60;
+      int min = days * 24 * 60;
 
-int sec = days * 24 * 60 * 60;
+      int sec = days * 24 * 60 * 60;
 
-System.out.print("In " + days+ " " + "days it is " + hours +" hrs " + min + " minutes " + sec +" seconds");
-}
+       System.out.print("In " + days+ " " + "days it is " + hours +" hrs " + min + " minutes " + sec +" seconds");
+  }
 }
