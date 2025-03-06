ec1ef0f1e551a55aa57f68a387173a2b939c9ff3
Kaviya S <kaviya.s0806@gmail.com>
2025-03-06 23:57:47 +0530

    NumberCheck.java

---
+++ NumberCheck.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+  public class NumberCheck{
+
+    public static void main(String[] args){
+
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("Enter a number: ");
+
+      int n = sc.nextInt();
+
+    if(n % 2 != 0){
+      
+     System.out.println("weird");
+
+   }
+    else{
+
+        if(n >=2 && n <= 5){
+
+           System.out.println("Not Weird");
+       }
+
+        else if(n >= 6 && n <= 20){
+
+           System.out.println("Weird");
+
+        }
+
+        else if(n > 20) 
+            System.out.println("Not weird");
+
+   }
+  
+  }
+
+}
